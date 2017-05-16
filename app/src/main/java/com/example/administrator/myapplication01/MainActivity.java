4b3318f52671091ac4554aa1e3515a6c147e8fea
ybb <2207877178@qq.com>
2017-05-16 14:51:03 +0800

    01

--- app/src/main/java/com/example/administrator/myapplication01/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication01/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.administrator.myapplication01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Unbinder mUnbinder;
+
+    @BindView(R.id.btn_01)
+    Button btn_01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mUnbinder = ButterKnife.bind(this);
+        init();
+
+    }
+
+    private void init() {
+        btn_01.setOnClickListener(v -> {
+            Toast.makeText(this, "hahhah", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mUnbinder.unbind();
     }
 }
